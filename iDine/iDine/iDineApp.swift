f87f85c98696374bae4883ce1abc646c483aaae4
Okan <orkunokann@gmail.com>
2024-07-08 11:06:55 +0300

    iDine app created and added files, images

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Okan Orkun on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
