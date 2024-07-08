3a872a0bc07d36ed4b33db89ccc6535af64c0dec
Okan <orkunokann@gmail.com>
2024-07-08 11:26:39 +0300

    Composing views to create a list row

---
+++ iDine/iDine/ItemRow.swift
@@ -0,0 +1,28 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Okan Orkun on 8.07.2024.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ItemRow(item: .example)
+}
