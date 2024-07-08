694e89786d34b201f48f2a7e51d1ed7280b62bae
Okan <orkunokann@gmail.com>
2024-07-08 13:26:38 +0300

    Calculated total price with tip percentage

--- iDine/iDine/CheckoutView.swift
+++ iDine/iDine/CheckoutView.swift
@@ -18,6 +18,12 @@ struct CheckoutView: View {
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     let tipAmounts = [10, 15, 20, 25, 0]
     
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
         Form {
             Section {
@@ -37,13 +43,13 @@ struct CheckoutView: View {
             Section("Add a tip?") {
                 Picker("Percentage", selection: $tipAmount) {
                     ForEach(tipAmounts, id: \.self) {
-                        Text("\($0)")
+                        Text("\($0)%")
                     }
                 }
                 .pickerStyle(.segmented)
             }
             
-            Section("Total: $100") {
+            Section("Total: \(totalPrice)") {
                 Button("Confirm order") {
                     
                 }
