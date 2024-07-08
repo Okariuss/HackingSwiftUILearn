bc5a0079d5ffeeef2b14a6cafc94684354cbd554
Okan <orkunokann@gmail.com>
2024-07-08 20:04:55 +0300

    GuessTheFlags app created and flags downloaded

---
+++ GuessTheFlags/GuessTheFlags/GuessTheFlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagsApp.swift
+//  GuessTheFlags
+//
+//  Created by Okan Orkun on 8.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
