e7bad15f494ffa3a085d20871d888606c1748808
Okan <orkunokann@gmail.com>
2024-07-09 14:23:40 +0300

    BetterRest Project created

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Okan Orkun on 9.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
