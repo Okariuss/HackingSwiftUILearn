b6475686d433bdba9da732753f9718d694aa8d75
Okan <orkunokann@gmail.com>
2024-07-09 20:20:52 +0300

    WordScramble app completed

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,13 +12,29 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    @State private var score = 0
+    @FocusState private var isFocused: Bool
     
     var body: some View {
         NavigationStack {
+            
+            HStack {
+                Text("Found: \(usedWords.count) word")
+                    .font(.headline)
+                Spacer()
+                Text("Score: \(score)")
+                    .font(.title)
+            }
+            .padding()
+            
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
                         .textInputAutocapitalization(.never)
+                        .focused($isFocused)
                 }
                 
                 Section {
@@ -33,6 +49,18 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) { } message: {
+                Text(errorMessage)
+            }
+            .toolbar {
+                Button("Restart") {
+                    restart()
+                    isFocused = false
+                }
+            }
+            .onTapGesture {
+                isFocused = false
+            }
         }
     }
     
@@ -41,8 +69,33 @@ struct ContentView: View {
         
         guard answer.count > 0 else { return }
         
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            newWord = ""
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell \(newWord) from '\(rootWord)'")
+            newWord = ""
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            newWord = ""
+            return
+        }
+        
+        guard isValidWord(word: answer) else {
+            wordError(title: "Invalid answer", message: "Answers must be at least three letters long and cannot be the same as the start word.")
+            newWord = ""
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
+            score += answer.count
         }
         newWord = ""
     }
@@ -64,6 +117,48 @@ struct ContentView: View {
         
         // If were are here then there was a problem - trigger crash and report the error
         fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func isValidWord(word: String) -> Bool {
+        return rootWord != word && word.count >= 3
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func restart() {
+        startGame()
+        score = 0
+        usedWords.removeAll()
+        newWord = ""
     }
 }
 
