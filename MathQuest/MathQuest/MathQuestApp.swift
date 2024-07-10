39ecf497e2aa1e3a987d37a9d6e00a1aab0df3c2
Okan <orkunokann@gmail.com>
2024-07-10 09:50:21 +0300

    Created a new project MathQuest

---
+++ MathQuest/MathQuest/MathQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathQuestApp.swift
+//  MathQuest
+//
+//  Created by Okan Orkun on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MathQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
