9c7eed5b699b928a79dd43797365c809391ea918
Okan <orkunokann@gmail.com>
2024-07-10 14:56:57 +0300

    iExpense project created

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Okan Orkun on 10.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
