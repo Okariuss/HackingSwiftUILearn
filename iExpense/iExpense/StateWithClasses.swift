9d2de27428d3ea80c91b402a38f9edefe335c927
Okan <orkunokann@gmail.com>
2024-07-10 15:18:01 +0300

    Used @State with classes using @Observable

---
+++ iExpense/iExpense/StateWithClasses.swift
@@ -0,0 +1,29 @@
+//
+//  StateWithClasses.swift
+//  iExpense
+//
+//  Created by Okan Orkun on 10.07.2024.
+//
+
+import SwiftUI
+import Observation
+
+struct StateWithClasses: View {
+    
+    @State private var user = User()
+    
+    var body: some View {
+        
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First Name", text: $user.firstName)
+            TextField("Last Name", text: $user.lastName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    StateWithClasses()
+}
---
+++ iExpense/iExpense/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  iExpense
+//
+//  Created by Okan Orkun on 10.07.2024.
+//
+
+import Foundation
+
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
