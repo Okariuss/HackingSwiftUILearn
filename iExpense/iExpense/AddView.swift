3cc7c23db3ba2547e06db414a3422d53a230cf0b
Okan <orkunokann@gmail.com>
2024-07-11 11:54:01 +0300

    Sharing observed object with a new view

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Okan Orkun on 11.07.2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    var expenses: Expenses
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
