1be2db56f1c23672e340af5799db2e728ae4d290
Okan <orkunokann@gmail.com>
2024-07-11 12:09:41 +0300

    Making changes permanent with UserDefaults

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -13,7 +13,7 @@ struct AddView: View {
     
     @State private var name = ""
     @State private var type = "Personal"
-    @State private var amount = 0
+    @State private var amount = 0.0
     
     let types = ["Business", "Personal"]
     
@@ -32,6 +32,12 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
         }
     }
 }
