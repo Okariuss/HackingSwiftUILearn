99c35bf30b667a878a5fde97b48f4e9a556063cf
Okan <orkunokann@gmail.com>
2024-07-12 00:54:03 +0300

    Merging Codable structs

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -8,7 +8,25 @@
 import SwiftUI
 
 struct MissionView: View {
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
     let mission: Mission
+    let crew: [CrewMember]
+    
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
     
     var body: some View {
         ScrollView {
@@ -29,6 +47,37 @@ struct MissionView: View {
                     Text(mission.description)
                 }
                 .padding(.horizontal)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text("Astronaut details")
+                            } label: {
+                                HStack {
+                                    Image(crewMember.astronaut.id)
+                                        .resizable()
+                                        .frame(width: 104, height: 72)
+                                        .clipShape(.capsule)
+                                        .overlay(
+                                            Capsule()
+                                                .strokeBorder(.white, lineWidth: 1)
+                                        )
+                                    
+                                    VStack(alignment: .leading) {
+                                        Text(crewMember.astronaut.name)
+                                            .foregroundStyle(.white)
+                                            .font(.headline)
+                                        Text(crewMember.role)
+                                            .foregroundStyle(.white.opacity(0.5))
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+
+                        }
+                    }
+                }
             }
             .padding(.bottom)
         }
@@ -41,7 +90,8 @@ struct MissionView: View {
 #Preview {
     
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
-    return MissionView(mission: missions[2])
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    return MissionView(mission: missions[2], astronauts: astronauts)
         .preferredColorScheme(.dark)
 }
