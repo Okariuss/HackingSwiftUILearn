c526d3e6c17f07f9c611d8876046a85725fb7790
Okan <orkunokann@gmail.com>
2024-07-12 00:34:15 +0300

    Create MissionView
    Showing mission details with ScrollView and containerRelativeFrame()

---
+++ Moonshot/Moonshot/MissionView.swift
@@ -0,0 +1,47 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Okan Orkun on 12.07.2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                    .padding(.top)
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[2])
+        .preferredColorScheme(.dark)
+}
