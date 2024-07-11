7114d8d7ce6aaa34a6ec89f2c07284fa552b44f4
Okan <orkunokann@gmail.com>
2024-07-11 20:11:00 +0300

    Learned ScrollView with LazyHStack and LazyVStack

---
+++ Moonshot/Moonshot/ScrollViewLearn.swift
@@ -0,0 +1,50 @@
+//
+//  ScrollViewLearn.swift
+//  Moonshot
+//
+//  Created by Okan Orkun on 11.07.2024.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
+struct ScrollViewLearn: View {
+    var body: some View {
+        
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+//        ScrollView {
+//            LazyVStack(spacing: 10) {
+//                ForEach(0..<100) {
+//                    CustomText("Item \($0)")
+//                        .font(.title)
+//                }
+//            }
+//            .frame(maxWidth: .infinity)
+//        }
+    }
+}
+
+#Preview {
+    ScrollViewLearn()
+}
