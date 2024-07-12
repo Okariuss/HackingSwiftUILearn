837f3bfbdcf3fe6e2d781da43bbf4058e841c5ad
Okan <orkunokann@gmail.com>
2024-07-12 15:28:13 +0300

    HabitTracker app created

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Okan Orkun on 12.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
