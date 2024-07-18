636a2af8dd2cf8365f03da5408e056322ada1753
Okan <orkunokann@gmail.com>
2024-07-18 10:56:39 +0300

    CupcakeCorner project created

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Okan Orkun on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
