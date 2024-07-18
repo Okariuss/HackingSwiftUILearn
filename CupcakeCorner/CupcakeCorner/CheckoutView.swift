f8b76d1cc76d2f85b49ed07cf8472f3b5d90d9d0
Okan <orkunokann@gmail.com>
2024-07-18 13:03:16 +0300

    Checked for a valid address

---
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Okan Orkun on 18.07.2024.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text("Hello, World")
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
