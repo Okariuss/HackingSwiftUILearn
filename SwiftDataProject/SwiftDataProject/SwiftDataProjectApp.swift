7d7d976b2ce564be84fb0a810f5411a95ff8cace
Okan <orkunokann@gmail.com>
2024-07-20 13:51:56 +0300

    SwiftDataProject created

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Okan Orkun on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
