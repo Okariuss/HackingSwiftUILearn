4fc5a0d9138f089d0da26721d4cefc00c53d37b6
Okan <orkunokann@gmail.com>
2024-07-20 09:58:05 +0300

    Bookworm project created

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Okan Orkun on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
