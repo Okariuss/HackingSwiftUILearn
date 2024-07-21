40cdc999cad4698fc2fbc0761e69df15d5d8442a
Okan <orkunokann@gmail.com>
2024-07-21 12:57:20 +0300

    basic UI created for project

---
+++ Instafilter/Instafilter/HomePage.swift
@@ -0,0 +1,55 @@
+//
+//  HomePage.swift
+//  Instafilter
+//
+//  Created by Okan Orkun on 21.07.2024.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // share picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+        }
+    }
+    
+    func changeFilter() {
+        
+    }
+}
+
+#Preview {
+    HomePage()
+}
