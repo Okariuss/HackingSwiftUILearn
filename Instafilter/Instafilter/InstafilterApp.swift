88b30289f29b4283066754767b6233dd3bb7eef9
Okan <orkunokann@gmail.com>
2024-07-21 11:11:19 +0300

    Instafilter project created

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Okan Orkun on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
