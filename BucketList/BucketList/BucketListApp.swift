e7ee581de424de516fd1e927fee85ffebe395e1a
Okan <orkunokann@gmail.com>
2024-07-21 17:32:13 +0300

    BucketList project created

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Okan Orkun on 21.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
