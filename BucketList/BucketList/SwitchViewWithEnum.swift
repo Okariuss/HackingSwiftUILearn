96b727c843aa8363f81073b10b3b96122b961979
Okan <orkunokann@gmail.com>
2024-07-21 19:53:17 +0300

    Switching view states with enums

---
+++ BucketList/BucketList/SwitchViewWithEnum.swift
@@ -0,0 +1,49 @@
+//
+//  SwitchViewWithEnum.swift
+//  BucketList
+//
+//  Created by Okan Orkun on 21.07.2024.
+//
+
+import SwiftUI
+
+struct SwitchViewWithEnum: View {
+    @State private var loadingState = LoadingState.loading
+    
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
+    }
+}
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+#Preview {
+    SwitchViewWithEnum()
+}
