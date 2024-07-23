1aee0749e4d358fc2d40051436fa1a91619cdbc8
Okan <orkunokann@gmail.com>
2024-07-23 17:10:38 +0300

    AccessibilitySandbox project created

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Okan Orkun on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
