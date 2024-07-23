9bd1da5ef954c5d9991e8b6456903773d3c4a23a
Okan <orkunokann@gmail.com>
2024-07-23 18:02:15 +0300

    Reading the value of controls

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        HidingAndGroupingData()
+        ReadingValueOfControls()
+    }
+}
+
+struct ReadingValueOfControls: View {
+    
+    @State private var value = 10
+    var body: some View {
+        VStack {
+            Text("Value \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled.")
+            }
+        }
     }
 }
 
