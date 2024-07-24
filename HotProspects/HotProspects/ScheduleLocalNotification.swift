4ada8fc845f421771674314799232d30f6bb0955
Okan <orkunokann@gmail.com>
2024-07-24 20:14:45 +0300

    Scheduling local notifications

---
+++ HotProspects/HotProspects/ScheduleLocalNotification.swift
@@ -0,0 +1,42 @@
+//
+//  ScheduleLocalNotification.swift
+//  HotProspects
+//
+//  Created by Okan Orkun on 24.07.2024.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ScheduleLocalNotification: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+}
+
+#Preview {
+    ScheduleLocalNotification()
+}
