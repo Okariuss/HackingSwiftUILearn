62292636515223f5b55aedb4e2757f4597e72aff
Okan <orkunokann@gmail.com>
2024-07-25 17:38:38 +0300

    Flashzilla project created

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Okan Orkun on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
