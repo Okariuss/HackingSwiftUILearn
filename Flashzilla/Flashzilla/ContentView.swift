7081960b0209bcaeeb2ba607999f963a710b507c
Okan <orkunokann@gmail.com>
2024-07-26 19:42:43 +0300

    Ending the app with allowsHitTesting

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -41,6 +41,15 @@ struct ContentView: View {
                         }
                         .stacked(at: index, in: cards.count)
                     }
+                    .allowsHitTesting(timeRemaining > 0)
+                    
+                    if cards.isEmpty {
+                        Button("Start Again", action: resetCards)
+                            .padding()
+                            .background(.white)
+                            .foregroundStyle(.black)
+                            .clipShape(.capsule)
+                    }
                 }
             }
             
@@ -74,7 +83,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) {
             if scenePhase == .active {
-                isActive = true
+                if !cards.isEmpty {
+                    isActive = true
+                }
             } else {
                 isActive = false
             }
@@ -83,6 +94,16 @@ struct ContentView: View {
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
