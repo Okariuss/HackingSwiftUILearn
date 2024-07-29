1c261603b63aa0152ebdecb3333be4a3d47de247
Okan <orkunokann@gmail.com>
2024-07-29 15:36:08 +0300

    How layout works in SwiftUI

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutWorks.swift
@@ -0,0 +1,24 @@
+//
+//  LayoutWorks.swift
+//  LayoutAndGeometry
+//
+//  Created by Okan Orkun on 29.07.2024.
+//
+
+import SwiftUI
+
+struct LayoutWorks: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding(20)
+            .background(.red)
+        
+        Text("Hello, World!")
+            .background(.red)
+            .padding(20)
+    }
+}
+
+#Preview {
+    LayoutWorks()
+}
