4d374468fbadd55169a1ec5343a38060ac58f685
Okan <orkunokann@gmail.com>
2024-07-29 13:05:59 +0300

    LayoutAndGeometry project created

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Okan Orkun on 29.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
