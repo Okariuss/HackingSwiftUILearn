d85df88ae8644f7597c028397956e4b59ac86845
Okan <orkunokann@gmail.com>
2024-07-30 07:55:15 +0300

    SnowSeeker project created

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Okan Orkun on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
